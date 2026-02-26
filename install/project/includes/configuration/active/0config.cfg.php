6d59f7b46bd2273c144cd3ae4bf462814b68d892
Tiit Papp <tiit.papp@gmail.com>
2026-02-26 10:38:24 +0200

    Rename constant __DESIGN_MODE__ to DESIGN_MODE

--- install/project/includes/configuration/active/0config.cfg.php
+++ install/project/includes/configuration/active/0config.cfg.php
@@ -82,7 +82,7 @@ switch (SERVER_INSTANCE) {
         define ('QCUBED_FORMS_DIR', QCUBED_PROJECT_DIR . '/forms');
 
         define ('ALLOW_REMOTE_ADMIN', true);
-        define ('__DESIGN_MODE__', 1);
+        define ('DESIGN_MODE', 1);
         break;
 
     case 'test':
